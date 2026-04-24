9fe51383300f49d3d7ca50ffccd4fd195e03149d
Sabribrina <sabrinal117@nycstudents.net>
2026-04-24 10:16:50 -0400

    4/24/26

--- wowow.py
+++ wowow.py
@@ -1,17 +1,18 @@
 import requests
+import json
 
-def getPoke(poke):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+def getEmoji(sobsob):
+    response = requests.get(f"https://github.com/cheatsnake/emojihub/commit/ac8740a89a4bad46a9c40ef44f4be5b13f33bd37#diff-f52e6975c1fff64036e70914311e48390fdcd30e8cfb3f1ac0a41b1083462f3f{sobsob.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
+
     data = response.json()
     return {
         "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
+        "category": data["category"],
+        "group": data["group"]
     }
 
-pokemon = getPoke("Bulbasaur")
-print(pokemon)
+emoji = getEmoji("china")
+print(emoji)
