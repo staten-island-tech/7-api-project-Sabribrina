494e65961ecfb44fbf3b2b9304b9f2b62adf296f
Sabribrina <sabrinal117@nycstudents.net>
2026-04-27 10:17:20 -0400

    4/27/26

--- wowow.py
+++ wowow.py
@@ -1,18 +1,72 @@
 import requests
 import json
 
-def getEmoji(sobsob):
-    response = requests.get(f"https://github.com/cheatsnake/emojihub/commit/ac8740a89a4bad46a9c40ef44f4be5b13f33bd37#diff-f52e6975c1fff64036e70914311e48390fdcd30e8cfb3f1ac0a41b1083462f3f{sobsob.lower()}")
+def getCreature(sobsob):
+    response = requests.get(f"docs.api.jikan.moe/anime/{id}/full{sobsob.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
 
     data = response.json()
     return {
-        "name": data["name"],
+        "titles": data["titles"],
         "category": data["category"],
         "group": data["group"]
     }
 
-emoji = getEmoji("china")
-print(emoji)
+creature = getCreature("Pacific Spiny Dogfish")
+print(creature)
+
+"data": {
+    "mal_id": 0,
+    "url": "string",
+    "images": { … },
+    "trailer": { … },
+    "approved": true,
+    "titles": [ … ],
+    "title": "string",
+    "title_english": "string",
+    "title_japanese": "string",
+    "title_synonyms": [ … ],
+    "type": "TV",
+    "source": "string",
+    "episodes": 0,
+    "status": "Finished Airing",
+    "airing": true,
+    "aired": { … },
+    "duration": "string",
+    "rating": "G - All Ages",
+    "score": 0.1,
+    "scored_by": 0,
+    "rank": 0,
+    "popularity": 0,
+    "members": 0,
+    "favorites": 0,
+    "synopsis": "string",
+    "background": "string",
+    "season": "summer",
+    "year": 0,
+    "broadcast": { … },
+    "producers": [ … ],
+    "licensors": [ … ],
+    "studios": [ … ],
+    "genres": [ … ],
+    "explicit_genres": [ … ],
+    "themes": [ … ],
+    "demographics": [ … ],
+    "relations": [ … ],
+    "theme": { … },
+    "external": [ … ],
+    "streaming": [ … ]
+  }
+}
+
+""" def divide(a,b):
+    try:
+        result = a/ b
+    except ZeroDivisionError:
+        print("Cannot divide by 0")
+    else:
+        print(a/b)
+
+divide (10,0) """
