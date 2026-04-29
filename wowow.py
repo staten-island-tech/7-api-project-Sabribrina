9df7cc02557aa64a6e321f17f824664f8562d174
Sabribrina <sabrinal117@nycstudents.net>
2026-04-29 10:17:13 -0400

    4/29/26

--- wowow.py
+++ wowow.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def getChara(sobsob):
-    response = requests.get(f"https://api.disneyapi.dev/character{sobsob.lower()}")
+    response = requests.get(f"https://api.disneyapi.dev/character/{sobsob.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
@@ -10,5 +10,13 @@ def getChara(sobsob):
     data = response.json()
     print(data)
 
-chara = getChara("")
-print(chara)
+chara = getChara("291")
+
+for key, value in chara.items():
+    print(key, "→", value)
+
+""" chara = {
+    "name": "Aqua",
+    "url": "https://static.wikia.nocookie.net/disney/images/0/0a/Aqua_KHIII.png",
+}
+print(chara["name"]) """
